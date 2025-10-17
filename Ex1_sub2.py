3f9a608670db7ad3ac38bea33138402fdf97f81f
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 14:06:36 +0200

    8

--- Ex1_sub2.py
+++ Ex1_sub2.py
@@ -9,7 +9,10 @@ print ("Running ...")
 
 lefspeed = 43
 rightspeed = 90
-print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
-sleep(7)
-print(arlo.go_diff(rightspeed, lefspeed, 1, 1))  # Forward
-sleep(8)
+i = 3
+while i < 3: 
+    print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
+    sleep(6.85)
+    print(arlo.go_diff(rightspeed, lefspeed, 1, 1))  # Forward
+    sleep(8)
+    i += 1
