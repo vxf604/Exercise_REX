78ed56b3f877e54945f47a7b536abdc8a29e44ce
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 14:00:11 +0200

    8

---
+++ Ex1_sub2.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+import robot
+
+arlo = robot.Robot()
+
+print ("Running ...")
+
+
+lefspeed = 83
+rightspeed = 50
+print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
+sleep(2)
