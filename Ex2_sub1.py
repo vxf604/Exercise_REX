efef1614f21971fcaf4fc6a336a5f0c9f55fba38
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 15:18:45 +0200

    up

--- Ex2_sub1.py
+++ Ex2_sub1.py
@@ -11,13 +11,13 @@ while driving:
     print (arlo.go_diff(80, 80, 1, 1))  # Forward
     if arlo.read_front_ping_sensor() < 300:
         arlo.rotate_robot(90)
-        print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        arlo.go_diff(80, 80, 1, 1)
     elif arlo.read_left_ping_sensor() < 300:
         arlo.rotate_robot(-90)
-        print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        arlo.go_diff(80, 80, 1, 1)  # Forward
     elif arlo.read_right_ping_sensor() < 300:
         arlo.rotate_robot(90)
-        print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        arlo.go_diff(80, 80, 1, 1)  # Forward
         
         
 print ("Finished")
