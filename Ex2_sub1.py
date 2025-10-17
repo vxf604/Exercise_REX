e24007cd94044cb783302144521a3705d2e249c2
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 15:16:22 +0200

    up

--- Ex2_sub1.py
+++ Ex2_sub1.py
@@ -9,13 +9,13 @@ print ("Running ...")
 driving = True
 while driving:
     print (arlo.go_diff(80, 80, 1, 1))  # Forward
-    if arlo.read_front_ping_sensor() < 200:
+    if arlo.read_front_ping_sensor() < 300:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
-    elif arlo.read_left_ping_sensor() < 200:
+    elif arlo.read_left_ping_sensor() < 300:
         arlo.rotate_robot(-90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
-    elif arlo.read_right_ping_sensor() < 200:
+    elif arlo.read_right_ping_sensor() < 300:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
         
