98325d1cc489f6013d6d3df67a825ec56651a66a
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 13:44:42 +0200

    b

--- Ex1.py
+++ Ex1.py
@@ -9,8 +9,16 @@ print ("Running ...")
 lefspeed = 84
 rightspeed = 80
 
-print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
-sleep(2)
+# print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
+# sleep(2)
 
-print(arlo.go_diff(lefspeed, rightspeed, 0, 1))  # Turn right
-sleep(0.6)
+# print(arlo.go_diff(lefspeed, rightspeed, 0, 1))  # Turn right
+# sleep(0.6)
+
+
+for i in range (5):
+    print(arlo.go_diff(lefspeed, rightspeed, 1, 1))  # Forward
+    sleep(2)
+    print(arlo.go_diff(lefspeed, rightspeed, 0, 1))  # Turn right
+    sleep(0.6)
+    
