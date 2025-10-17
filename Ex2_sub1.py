3e61988db9bfc095ff9ecb00a601d2c192178ca8
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 15:11:05 +0200

    up

--- Ex2_sub1.py
+++ Ex2_sub1.py
@@ -6,22 +6,20 @@ arlo = robot.Robot()
 
 print ("Running ...")
 
-i = 0
-while i < 5:
+driving = True
+while driving:
     if arlo.read_front_ping_sensor() < 200:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
-        i += 1
     elif arlo.read_left_ping_sensor() < 200:
         arlo.rotate_robot(-90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
-        i += 1
     elif arlo.read_right_ping_sensor() < 200:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
-        i
     else:
-        print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        driving = False
+        
         
         
 print ("Finished")
