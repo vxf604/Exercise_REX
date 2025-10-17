b8782924a121b1fcf5d2b0e7786a2ff829b86f9d
vxf604 <vxf604@alumni.ku.dk>
2025-10-17 15:04:58 +0200

    up

--- Ex2_sub1.py
+++ Ex2_sub1.py
@@ -6,17 +6,22 @@ arlo = robot.Robot()
 
 print ("Running ...")
 
-for i in range (5):
+i = 0
+while i < 5:
     if arlo.read_front_ping_sensor() < 20:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        i += 1
     elif arlo.read_left_ping_sensor() < 20:
         arlo.rotate_robot(-90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        i += 1
     elif arlo.read_right_ping_sensor() < 20:
         arlo.rotate_robot(90)
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        i
     else:
         print(arlo.go_diff(80, 80, 1, 1))  # Forward
+        
         
 print ("Finished")
