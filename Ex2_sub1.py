013621d1e8738910ea9f07871804782a1d334738
vxf604 <vxf604@alumni.ku.dk>
2025-10-18 14:12:17 +0200

    up

--- Ex2_sub1.py
+++ Ex2_sub1.py
@@ -10,15 +10,15 @@ driving = True
 while driving:
     print (arlo.go_diff(80, 80, 1, 1))  # Forward
     if arlo.read_front_ping_sensor() < 300:
-        arlo.rotate_robot(360)
-        sleep(0.5)
-        arlo.go_diff(80, 80, 0, 1)
-    elif arlo.read_left_ping_sensor() < 300:
         arlo.rotate_robot(180)
         sleep(0.5)
         arlo.go_diff(80, 80, 0, 1)
+    elif arlo.read_left_ping_sensor() < 300:
+        arlo.rotate_robot(90)
+        sleep(0.5)
+        arlo.go_diff(80, 80, 0, 1)
     elif arlo.read_right_ping_sensor() < 300:
-        arlo.rotate_robot(-180)
+        arlo.rotate_robot(-90)
         sleep(0.5)
         arlo.go_diff(80, 80, 1, 0)
         
